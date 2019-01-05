2f5ad76bbbc6689e1f4403a75df386ae0b8e9fe5
Charlie Paull <charliepaull211@gmail.com>
2019-01-05 13:22:58 -0800

    git cloned, some files made, beg. of database made w/ sql.

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("3x3, animal style, extra chilis", FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ("double-double with grilled onions, no pickle", FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ("2 patty cheese burger, BBQ sauce, grilled pineapple", FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Father's Office Burger, no substitutions", FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ("double Flying Dutchman", FALSE);
